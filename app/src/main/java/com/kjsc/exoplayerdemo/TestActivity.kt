cc9c79b47e0803d2e70169de04a868546af98ece
heb <qq7476160>
2022-06-28 13:06:45 +0800

    ++++++++++++++++++++++

--- app/src/main/java/com/kjsc/exoplayerdemo/TestActivity.kt
+++ app/src/main/java/com/kjsc/exoplayerdemo/TestActivity.kt
@@ -1,12 +1,19 @@
 package com.kjsc.exoplayerdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.FragmentActivity
 
 class TestActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+    }
+
+    fun jump(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
