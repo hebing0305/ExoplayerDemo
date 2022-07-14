a060d3e7fdb06b34c829adfcb4e450625558e3f2
heb <qq7476160>
2022-07-14 10:37:42 +0800

    ++++++++++++++++++++++

--- app/src/main/java/com/kjsc/exoplayerdemo/TestActivity.kt
+++ app/src/main/java/com/kjsc/exoplayerdemo/TestActivity.kt
@@ -1,7 +1,6 @@
 package com.kjsc.exoplayerdemo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.FragmentActivity
